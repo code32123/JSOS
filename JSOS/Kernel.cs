8ffe9368bd862a9d53110f3e7c173a0b25136a8a
= <jimmy.l.smythe@gmail.com>
2023-02-09 23:28:37 -0800

    Color command!

--- JSOS/Kernel.cs
+++ JSOS/Kernel.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 using System.Text;
 using System.Threading;
 using Sys = Cosmos.System;
@@ -24,7 +27,7 @@ namespace JSOS
 			Console.Write("Beginning sys-check ");									// Write out the syscheck
 			for (int i = 0; i <= 100; ++i)	//	Sets i to 0, then adds one to it (++i) while the condition is met (i <= 100)
 			{
-				Thread.Sleep(4);													// Delays for 8 milliseconds
+				//Thread.Sleep(4);													// Delays for 8 milliseconds
 				Console.Write("\rBeginning sys-check {0}%   ", i);					// '\r' returns to the beginning of the line, so all previous
 																					// messages are overwritten with the new percentage
 			}
@@ -37,30 +40,109 @@ namespace JSOS
 			// In linux, the prompt is usually the current directory, but we havn't set that up yet. For now, just do this:
 			Console.Write("JSOS> "); // This is the 'prompt', or what comes right before the text
 
-			var input = Console.ReadLine();                                         // Gets user input and saves it to the input variable
+			string input = Console.ReadLine();                                         // Gets user input and saves it to the input variable
+			List<string> args = input.Split().ToList();
+			string command = args[0];
+			args.RemoveAt(0);
 
-			// Here is where we can use the variable 'input'. Currently, it contains whatever the user typed
-			// We can use the 'if' code to check what they typed.
-
-			if (input == "sysinfo") {
+			if (command == "sysinfo")
+			{
 				Console.WriteLine("This computer's information:");
-				Console.WriteLine("	Hardware:");
-				Console.WriteLine("		RAM:            " + Cosmos.Core.CPU.GetAmountOfRAM().ToString() + "MB");
-				Console.WriteLine("		CPU Brand:      " + Cosmos.Core.CPU.GetCPUBrandString());
-				Console.WriteLine("		CPU Vendor:     " + Cosmos.Core.CPU.GetCPUVendorName());
-				Console.WriteLine("		CPU Speed:      " + (Cosmos.Core.CPU.GetCPUCycleSpeed()/100000000).ToString() + "Ghz");
-				Console.WriteLine("	Operating System:");
-				Console.WriteLine("		Name:           JSOS, Jimmy S. Operating System");
-				Console.WriteLine("		Version:        v0.1.0");
-				Console.WriteLine("		Build Date:     8/2/2023");
-			} else if (input == "help") { // Here's where we can check. If it is not sysinfo, it might be help. However, if it's not help we keep checking
-				// until we get to either a 
-				Console.WriteLine("Currently loaded commands:");
-				Console.WriteLine("	help                Prints out this system help message");
-				Console.WriteLine("	sysinfo             Lists hardware and software commands");
-			} else { // Here is the else, what we do if nothing matches
-				Console.WriteLine("SYSERR: Command '" + input + "' not found on the system.");
+				Console.WriteLine("   Hardware:");
+				Console.WriteLine("      RAM:            " + Cosmos.Core.CPU.GetAmountOfRAM().ToString() + "MB");
+				Console.WriteLine("      CPU Brand:      " + Cosmos.Core.CPU.GetCPUBrandString());
+				Console.WriteLine("      CPU Vendor:     " + Cosmos.Core.CPU.GetCPUVendorName());
+				Console.WriteLine("      CPU Speed:      " + (Cosmos.Core.CPU.GetCPUCycleSpeed() / 100000000).ToString() + "Ghz");
+				Console.WriteLine("      CPU Uptime:     " + (Cosmos.Core.CPU.GetCPUUptime() / (ulong)Cosmos.Core.CPU.GetCPUCycleSpeed()).ToString() + "s");
+				Console.WriteLine("   Operating System:");
+				Console.WriteLine("      Name:           JSOS, Jimmy S. Operating System");
+				Console.WriteLine("      Version:        v0.1.0");
+				Console.WriteLine("      Build Date:     8/2/2023");
+			} else if (command == "clear") {
+				Console.Clear();
+			} else if (command == "color") {
+				int fgPos = myIndexOf(args, "-fg");
+				if (fgPos != -1) {
+					Console.ForegroundColor = getColor(args[fgPos + 1].ToString());
+				}
+				int bgPos = myIndexOf(args, "-bg");
+				if (bgPos != -1) {
+					Console.BackgroundColor = getColor(args[bgPos + 1].ToString());
+				}
+				Console.Clear();
+			} else if (command == "credits")
+			{
+				Console.WriteLine("This project was made as a group broject between:");
+				Console.WriteLine("Dion:");
+				Console.WriteLine("   Github: evildion07");
+				Console.WriteLine("   Discord: Musical Eevee#6926");
+				Console.WriteLine("Jimmy: ");
+				Console.WriteLine("   Github: code32123");
+				Console.WriteLine("   Discord: Jimmy32#2036");
 			}
+			else if (command == "help")
+			{
+				Console.WriteLine("Currently loaded commands:");
+				Console.WriteLine("   help                Prints out this system help message");
+				Console.WriteLine("   sysinfo             Lists hardware and software commands");
+				Console.WriteLine("   clear               Clears the screen");
+				Console.WriteLine("   credits             lists contact info for creators");
+			}
+			else
+			{ // Here is the else, what we do if nothing matches
+				Console.WriteLine("SYSERR: Command '" + command + "' not found on the system.");
+			}
+		}
+		string debugList(List<string> stringList) {
+			return String.Join(", ", stringList.ToArray()); ;
+		}
+		ConsoleColor getColor(string colorName){
+			colorName = colorName.ToLower();
+			if (colorName == "black") {
+				return ConsoleColor.Black;
+			} else if (colorName == "white") {
+				return ConsoleColor.White;
+			} else if (colorName == "blue") {
+				return ConsoleColor.Blue;
+			} else if (colorName == "darkblue") {
+				return ConsoleColor.DarkBlue;
+			} else if (colorName == "cyan") {
+				return ConsoleColor.Cyan;
+			} else if (colorName == "darkcyan") {
+				return ConsoleColor.DarkCyan;
+			} else if (colorName == "gray") {
+				return ConsoleColor.Gray;
+			} else if (colorName == "darkgray") {
+				return ConsoleColor.DarkGray;
+			} else if (colorName == "green") {
+				return ConsoleColor.Green;
+			} else if (colorName == "darkgreen") {
+				return ConsoleColor.DarkGreen;
+			} else if (colorName == "magenta") {
+				return ConsoleColor.Magenta;
+			} else if (colorName == "darkmagenta") {
+				return ConsoleColor.DarkMagenta;
+			} else if (colorName == "red") {
+				return ConsoleColor.Red;
+			} else if (colorName == "darkred") {
+				return ConsoleColor.DarkRed;
+			} else if (colorName == "yellow") {
+				return ConsoleColor.Yellow;
+			} else if (colorName == "darkyellow") {
+				return ConsoleColor.DarkYellow;
+			} else {
+				return ConsoleColor.Black;
+			}
+		}
+		int myIndexOf(List<string> stringList, string Find) {
+			int at = -1;
+			for (int i = 0; i < stringList.Count(); i++) {
+				if (stringList[i] == Find) {
+					at = i;
+					break;
+				}
+			}
+			return at;
 		}
 	}
 }
